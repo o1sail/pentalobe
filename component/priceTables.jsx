f472bff34cc6d3a55459a02981d18bf44518cf22
o1sail <about.o1sail@gmail.com>
2022-04-03 20:40:50 +0900

    2022/04/03 5

--- component/priceTables.jsx
+++ component/priceTables.jsx
@@ -68,13 +68,14 @@ export default function PriceTables() {
                 }
 
                 select {
-                    width: 100%;
+                    //width: 100%;
+                    width: fit-content;
                     height: 100%;
                     margin: 0 auto;
 
                     position: absolute;
                     top: 0;
-                    //right: 0;
+                    right: 0;//
                     left: 0;
                     bottom: 0;
                     z-index: 10;
