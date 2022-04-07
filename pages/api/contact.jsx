b256eb4b3a630cdad54e3bcadfb05218618a242e
o1sail <about.o1sail@gmail.com>
2022-04-07 20:32:37 +0900

    2022/04/07 5

--- pages/api/contact.jsx
+++ pages/api/contact.jsx
@@ -12,9 +12,10 @@ export default async (req, res) => {
         },
     });
 
+    
     try {
+        // 管理人に送るお問い合わせメッセージ通知メール
         await transporter.sendMail({
-            // 管理人に送るお問い合わせメッセージ通知メール
             from: email,
             to: "pentalobe.repair@gmail.com",
             subject: `【お問い合わせ】${name}様より`,
@@ -36,6 +37,36 @@ export default async (req, res) => {
             <p>${otherMessage}</p>
             `,
         });
+
+        // ゲストに送る自動受付メール
+        await transporter.sendMail({
+            from: process.env.SMTP_USER,
+            to: email,
+            subject: `【お問い合わせ自動受付メール】`,
+            html: `
+            <p>
+                お問い合わせありがとうございます。<br>
+                以下の内容でお問い合わせを承りました。
+            </p>
+            <p>-----------------------------------------</p>
+            <p>お問い合わせ内容</p>
+            <p>【お名前】</p>
+            <p>${req.body.name}</p>
+            <p>【メールアドレス】</p>
+            <p>${req.body.email}</p>
+            <p>【修理モデルと色】</p>
+            <p>${req.body.model}</p>
+            <p>【修理内容もしくは不具合内容】</p>
+            <p>${htmlMessage}</p>
+            <p>【希望日時】</p>
+            <p>${req.body.date}</p>
+            <p>【希望修理場所】</p>
+            <p>${req.body.place}</p>
+            <p>【その他】</p>
+            <p>${htmlOtherMessage}</p>
+            <p>-----------------------------------------</p>
+            `,
+        });
     } catch (error) {
         return res
             .status(500)
@@ -43,6 +74,50 @@ export default async (req, res) => {
     }
     return res.status(200).json({ error: "" });
 };
+
+
+
+
+    // // ゲストに送る自動受付メール
+    // try {
+    //     await transporter.sendMail({
+    //         from: process.env.SMTP_USER,
+    //         to: email,
+    //         subject: `【お問い合わせ自動受付メール】`,
+    //         html: `
+    //         <p>
+    //             お問い合わせありがとうございます。<br>
+    //             以下の内容でお問い合わせを承りました。
+    //         </p>
+    //         <p>-----------------------------------------</p>
+    //         <p>お問い合わせ内容</p>
+    //         <p>【お名前】</p>
+    //         <p>${req.body.name}</p>
+    //         <p>【メールアドレス】</p>
+    //         <p>${req.body.email}</p>
+    //         <p>【修理モデルと色】</p>
+    //         <p>${req.body.model}</p>
+    //         <p>【修理内容もしくは不具合内容】</p>
+    //         <p>${htmlMessage}</p>
+    //         <p>【希望日時】</p>
+    //         <p>${req.body.date}</p>
+    //         <p>【希望修理場所】</p>
+    //         <p>${req.body.place}</p>
+    //         <p>【その他】</p>
+    //         <p>${htmlOtherMessage}</p>
+    //         <p>-----------------------------------------</p>
+    //         `,
+    //     });
+    // } catch (error) {
+    //     return res
+    //         .status(500)
+    //         .json({ error: error.message || error.toString() });
+    // }
+    // return res.status(200).json({ error: "" });
+
+
+
+
 
 // export default function sendmail(req, res) {
 //     //　改行のエスケープシーケンスをbrタグに置換
